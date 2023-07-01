9cca9de7bc55ea08ba96c19db9fffe840a342b4b
hayelom kiros <hayelom.kiros@adey-meselesh.de>
2023-07-01 09:40:11 +0300

    creating app icon for the app

--- frontend/api/contstants.js
+++ frontend/api/contstants.js
@@ -1,2 +1,3 @@
-export default ENDPOINT = "http://192.168.0.118:8000";
+export default ENDPOINT = "https://book-store-0aph.onrender.com";
+//  "http://192.168.0.118:8000";
 //  "https://dummyjson.com/products";
